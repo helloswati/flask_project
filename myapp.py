5f9062e3c2cf4c6fa3ab569dceb5d562bd81b118
swati <swatigupta7@outook.in>
2022-07-06 21:42:05 +0530

    added more api

--- myapp.py
+++ myapp.py
@@ -6,7 +6,7 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route('/ml')
+#@app.route('/ml')
 def machine_learning():
     return'Machine learning is a branch of artificial intelligence (AI) and computer science which focuses on the use of data and algorithms to imitate the way that humans learn, gradually improving its accuracy.'
 
@@ -16,6 +16,16 @@ def NLP():
     return'NLP is a part of Computer Science and Artificial Intelligence which deals with human languages.'
     
 
+app.add_url_rule('/ml', 'ml', machine_learning) 
+
+@app.route('/hello/<name>')
+def hello_name(name):
+    return 'Hello %s' % name
+
+@app.route('/number/<int:num>')
+def hello_number(num):
+    salary = 20000 + num
+    return 'salary is  %d' % salary
 
 
 if __name__ == '__main__':
